c3b4b70394a4fcfda9150b270ce80833b2318d36
janicecaldeira <janicecaldeira@gmail.com>
2021-10-06 20:30:33 -0300

    Atualizando com autenticação

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Body, Get, Post, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Post,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '.prisma/client';
 import { CreateUserDto } from './users.dto';
 import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UsersController {
   constructor(private service: UsersService) {}
