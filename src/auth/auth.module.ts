99056e09d5f032e40ebc14e4bd850720a37feaca
janicecaldeira <janicecaldeira@gmail.com>
2021-10-06 20:19:33 -0300

    Correções

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './jwt.constants';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'src/prisma.service';
 import { AuthController } from './auth.controller';
 
 @Module({
