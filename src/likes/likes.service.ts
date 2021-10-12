91fb1711ebbbe727a7b05d089a8bb2b7f30f1815
janicecaldeira <janicecaldeira@gmail.com>
2021-10-12 14:22:09 -0300

    Atualizando likes

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { Like, Prisma } from '@prisma/client';
+import { Like } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
+import { CreateLikeDto } from './likes.dto';
 
 @Injectable()
 export class LikesService {
   constructor(private db: PrismaService) {}
 
-  async create(data: Prisma.LikeCreateInput): Promise<Like> {
+  async create(data: CreateLikeDto): Promise<Like> {
     return this.db.like.create({
       data,
     });
